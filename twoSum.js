72088424fbd36b84c2f6a57ee65ffad269971300
Yashas <ranjithacharya681@gmail.com>
2024-06-28 00:09:26 +0530

    added

--- twoSum.js
+++ twoSum.js
@@ -3,3 +3,16 @@ const twoSum = (array, goal) => {
     for(let i = 0; i < array.length; i++){
         for(let j = i + 1; j < array.length; j++){
             if (array[i] + array[j] === goal) {
+                const twoSum = (array, goal) => {
+                    let indexes = [];
+                
+                    for(let i = 0; i < array.length; i++){
+                       for(let j = i + 1; j < array.length; j++){
+                          if (array[i] + array[j] === goal) {
+                        indexes.push(i);
+                        indexes.push(j);
+                          }
+                       }
+                    }
+                    return indexes;
+                }
